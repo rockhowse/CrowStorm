0cdf9bc29381b70561419e11190327e7238ae90d
Nate Rock <rockhowse@gmail.com>
2017-03-16 13:27:49 -0500

    Simple implementaion of root and static file routes now to extend to complete the use cases

--- crow_storm.cpp
+++ crow_storm.cpp
@@ -9,22 +9,27 @@
 #include <fstream>
 #include <streambuf>
 
+// used for simple "security" validation
+#include <algorithm>
+
 // global mime type map
-std::map<std::string,std::string> g_mime_types;
+std::map<std::string,std::string> g_content_types;
 
 /**
- * Check to see if a path exists
+ * Initialize known HTTP content types
  */
-inline bool path_exists(const std::string& name)
+void init_content_types()
 {
-  struct stat buffer;
-  return (stat (name.c_str(), &buffer) == 0);
+	g_content_types[".html"] 	= "text/html";
+	g_content_types[".js"] 		= "text/javascript";
+	g_content_types[".css"] 	= "text/css";
+	g_content_types["unkown"]  	= "application/unknown";
 }
 
 /**
- * Get the content type for a specific file
+ * Get the content type for a specific file based on extension
  */
-inline std::string pick_content_type(const std::string& file_path)
+inline std::string get_content_type(const std::string& file_path)
 {
 	std::string::size_type idx;
 	std::string extension;
@@ -36,46 +41,76 @@ inline std::string pick_content_type(const std::string& file_path)
 		extension = file_path.substr(idx+1);
 
 		// if we haven't found this known extension set to application/unknown
-		if(g_mime_types.find(extension) == g_mime_types.end())
+		if(g_content_types.find(extension) == g_content_types.end())
 		{
-			return g_mime_types["uknown"];
+			return g_content_types["uknown"];
 		}
 		else
 		{
-			return  g_mime_types[extension];
+			return  g_content_types[extension];
 		}
 	}
 	else
 	{
-	    return g_mime_types["uknown"];
+	    return g_content_types["uknown"];
 	}
 }
 
 /**
- * Initialize known mime types
+ * Check to see if a path exists
  */
-void init_mime_types()
+inline bool path_exists(const std::string& name)
 {
-	g_mime_types[".html"] 	= "text/html";
-	g_mime_types[".js"] 	= "text/javascript";
-	g_mime_types[".css"] 	= "text/css";
-	g_mime_types["unkown"]  = "application/unknown";
+  struct stat buffer;
+  return (stat (name.c_str(), &buffer) == 0);
 }
 
+/**
+ * Main crow based C++ HTTP server used for delivering the content needed by the CrowStorm platform
+ */
 int main()
 {
     crow::SimpleApp app;
 
-    //simple default route that returns crow-storm version
+    // *NOTE* ~ performance concern with static file loading
+    //          the files are loaded fully into memory before being returned
+    //          ideally, the bytes could be streamed from the disk/cache instead
+    //          this is especially important in large files
+
+    // Initialize supported content types
+    init_content_types();
+
+    //simple default route that returns index.html
     CROW_ROUTE(app, "/")
         .name("crow")
     ([]{
-        return "Crow Storm v0.0.1!";
+    	std::string full_path = "./public/index.html";
+		std::ifstream t(full_path);
+		std::stringstream buffer;
+		buffer << t.rdbuf();
+
+		crow::response r(buffer.str());
+		r.add_header("Content-Type", get_content_type(full_path));
+
+		return r;
     });
 
     // route for loading static files (html/javascript/css)
+    // VERY insecure as it does no URL validation and could be subject to
+    // reverse directory traversal or other URL based request
     CROW_ROUTE(app, "/<string>")
     ([](std::string path){
+
+    	// if there is ever '..', in the path, assume they are up to something
+    	// will catch simple attempts to use "../" to go backwards in the directory structure
+    	// will NOT catch more advanced encoding vulnerabilities (unicode, double unicode etc.)!
+    	if (path.find("..") != std::string::npos)
+    	{
+    		return crow::response(404);
+    	}
+
+    	// we are "safe" to continue loading a static file
+    	// for this simple example assume all static files live in the "./public/" directory
     	std::string full_path;
     	full_path =  "./public/" + path;
 
@@ -85,7 +120,7 @@ int main()
 			buffer << t.rdbuf();
 
 			crow::response r(buffer.str());
-			r.add_header("Content-Type", pick_content_type(full_path));
+			r.add_header("Content-Type", get_content_type(full_path));
 
 			return r;
         }
@@ -94,9 +129,8 @@ int main()
         }
     });
 
-    // ignore all log
+    // ignore all logging
     crow::logger::setLogLevel(crow::LogLevel::Debug);
-    //crow::logger::setHandler(std::make_shared<ExampleLogHandler>());
 
     app.port(18080)
         .multithreaded()
