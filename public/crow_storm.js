c8b2719ef2fa096a2ca70f8609b0fa4e75162b11
Nate Rock <rockhowse@gmail.com>
2017-03-18 20:02:38 -0500

    added advanced graphing componants, SMA + volume

--- public/crow_storm.js
+++ public/crow_storm.js
@@ -36,30 +36,30 @@ function drawGraphHeader(cur_btn_name, name, symbol, symbols) {
 	// add button handlers here
 	$.each(btn_list, function( index, btn_info ) {	 
 		
-		// if this is graph we are watching, set to "selected" and no click handler
+		// if this is graph we are watching, set to "selected"
 		if(btn_info[0] == cur_btn_name){
 			$("#" + btn_info[0]).addClass('active');
-		// otherwise, set the click handler so we can re-draw
-		} else {
-			$("#" + btn_info[0]).click(function() {
-				drawGraph(btn_info[0], btn_info[1], btn_info[2], btn_info[3]);
-			});
-		}
+		} 
+		
+		// keep click handler for ALL buttons in case we want to refresh
+		$("#" + btn_info[0]).click(function() {
+			drawGraph(btn_info[0], btn_info[1], btn_info[2], btn_info[3]);
+		});
 	});	
 }
 
-function drawGraph(cur_btn_name, name, symbol, symbols) {
 
-	$('#update').empty();
-	drawGraphHeader(cur_btn_name, name, symbol, symbols);
-	
-    clearChart();
-    
-	var margin = {top: 20, right: 20, bottom: 30, left: 50},
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
+function chart(name, symbol, fullWidth, fullHeight) {
+
+    var margin = {top: 20, right: 30, bottom: 30, left: 30},
+            width = fullWidth - margin.left - margin.right,
+            height = fullHeight - margin.top - margin.bottom,
+            volumeHeight = fullHeight*.25;
 
     var parseDate = d3.timeParse("%Y-%m-%d");
+
+    var zoom = d3.zoom()
+            .on("zoom", zoomed);
 
     var x = techan.scale.financetime()
             .range([0, width]);
@@ -67,28 +67,114 @@ function drawGraph(cur_btn_name, name, symbol, symbols) {
     var y = d3.scaleLinear()
             .range([height, 0]);
 
+    var yPercent = y.copy();   // Same as y at this stage, will get a different domain later
+
+    var yVolume = d3.scaleLinear()
+            .range([height, height - volumeHeight]);
+
+    var yInit, yPercentInit, zoomableInit;
+
     var candlestick = techan.plot.candlestick()
             .xScale(x)
             .yScale(y);
 
-    var xAxis = d3.axisBottom()
-            .scale(x);
+    var sma0 = techan.plot.sma()
+            .xScale(x)
+            .yScale(y);
 
-    var yAxis = d3.axisLeft()
-            .scale(y);
+    var sma1 = techan.plot.sma()
+            .xScale(x)
+            .yScale(y);
+
+    var ema2 = techan.plot.ema()
+            .xScale(x)
+            .yScale(y);
+
+    var volume = techan.plot.volume()
+            .accessor(candlestick.accessor())   // Set the accessor to a ohlc accessor so we get highlighted bars
+            .xScale(x)
+            .yScale(yVolume);
+
+    var xAxis = d3.axisBottom(x)
+            .ticks(4);
+
+    var yAxis = d3.axisRight(y)
+            .ticks(4);
+
+    var percentAxis = d3.axisLeft(yPercent)
+            .ticks(4)
+            .tickFormat(d3.format('+.1%'));
+
+    var volumeAxis = d3.axisRight(yVolume)
+            .ticks(2)
+            .tickFormat(d3.format(",.3s"));
 
     var svg = d3.select("#chart").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-            
-	d3.select("#chart").attr("align","center");
-	
-    d3.csv("/symbol/csv/" + symbol, function(error, data) {
-        var accessor = candlestick.accessor();
 
-        data = data.slice(0, 200).map(function(d) {
+	d3.select("#chart").attr("align","center");
+
+    svg.append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("x", 0)
+            .attr("y", y(1))
+            .attr("width", width)
+            .attr("height", y(0) - y(1));
+
+    svg.append('text')
+            .attr("class", "symbol")
+            .attr("x", 5)
+            .text(name + " (" + symbol + ")");
+
+    svg.append("g")
+            .attr("class", "volume")
+            .attr("clip-path", "url(#clip)");
+
+    svg.append("g")
+            .attr("class", "candlestick")
+            .attr("clip-path", "url(#clip)");
+
+    svg.append("g")
+            .attr("class", "indicator sma ma-0")
+            .attr("clip-path", "url(#clip)");
+
+    svg.append("g")
+            .attr("class", "indicator sma ma-1")
+            .attr("clip-path", "url(#clip)");
+
+    svg.append("g")
+            .attr("class", "indicator ema ma-2")
+            .attr("clip-path", "url(#clip)");
+
+    svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")");
+
+    svg.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(" + width + ",0)");
+
+    svg.append("g")
+            .attr("class", "percent axis");
+
+    svg.append("g")
+            .attr("class", "volume axis");
+
+    svg.append("rect")
+            .attr("class", "pane")
+            .attr("width", width)
+            .attr("height", height)
+            .call(zoom);
+
+    d3.csv("/symbol/csv/" + symbol, function (error, data) {
+        var accessor = candlestick.accessor(),
+            indicatorPreRoll = 1;  // Don't show where indicators don't have data
+
+        data = data.map(function (d) {
             return {
                 date: parseDate(d.Date),
                 open: +d.Open,
@@ -97,38 +183,71 @@ function drawGraph(cur_btn_name, name, symbol, symbols) {
                 close: +d.Close,
                 volume: +d.Volume
             };
-        }).sort(function(a, b) { return d3.ascending(accessor.d(a), accessor.d(b)); });
+        }).sort(function (a, b) {
+            return d3.ascending(accessor.d(a), accessor.d(b));
+        });
 
-        svg.append("g")
-                .attr("class", "candlestick");
+        x.domain(techan.scale.plot.time(data, accessor).domain());
+        y.domain(techan.scale.plot.ohlc(data.slice(indicatorPreRoll), accessor).domain());
+        yPercent.domain(techan.scale.plot.percent(y, accessor(data[indicatorPreRoll])).domain());
+        yVolume.domain(techan.scale.plot.volume(data, accessor.v).domain());
 
-        svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")");
+        svg.select("g.candlestick").datum(data).call(candlestick);
+        svg.select("g.volume").datum(data).call(volume);
+        svg.select("g.sma.ma-0").datum(techan.indicator.sma().period(10)(data)).call(sma0);
+        svg.select("g.sma.ma-1").datum(techan.indicator.sma().period(20)(data)).call(sma1);
+        svg.select("g.ema.ma-2").datum(techan.indicator.ema().period(50)(data)).call(ema2);
 
-        svg.append("g")
-                .attr("class", "y axis")
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text("Price ($)");
 
-        // Data to display initially
-        draw(data.slice(0, data.length));
-        // Only want this button to be active if the data has loaded
-        d3.select("button").on("click", function() { draw(data); }).style("display", "inline");
+        zoomableInit = x.zoomable().domain([indicatorPreRoll, data.length]).copy(); // Zoom in a little to hide indicator preroll
+        yInit = y.copy();
+        yPercentInit = yPercent.copy();
+
+        draw();
     });
 
-    function draw(data) {
-        x.domain(data.map(candlestick.accessor().d));
-        y.domain(techan.scale.plot.ohlc(data, candlestick.accessor()).domain());
-
-        svg.selectAll("g.candlestick").datum(data).call(candlestick);
-        svg.selectAll("g.x.axis").call(xAxis);
-        svg.selectAll("g.y.axis").call(yAxis);
+    function reset() {
+        zoom.scale(1);
+        zoom.translate([0, 0]);
+        draw();
     }
+
+    function zoomed() {
+        x.zoomable().domain(d3.event.transform.rescaleX(zoomableInit).domain());
+        y.domain(d3.event.transform.rescaleY(yInit).domain());
+        yPercent.domain(d3.event.transform.rescaleY(yPercentInit).domain());
+
+        draw();
+    }
+
+    function draw() {
+        svg.select("g.x.axis").call(xAxis);
+        svg.select("g.y.axis").call(yAxis);
+        svg.select("g.volume.axis").call(volumeAxis);
+        svg.select("g.percent.axis").call(percentAxis);
+
+        // We know the data does not change, a simple refresh that does not perform data joins will suffice.
+        svg.select("g.candlestick").call(candlestick.refresh);
+        svg.select("g.volume").call(volume.refresh);
+        svg.select("g.sma.ma-0").call(sma0.refresh);
+        svg.select("g.sma.ma-1").call(sma1.refresh);
+        svg.select("g.ema.ma-2").call(ema2.refresh);
+    }
+}
+
+
+
+
+
+
+function drawGraph(cur_btn_name, name, symbol, symbols) {
+
+	$('#update').empty();
+	drawGraphHeader(cur_btn_name, name, symbol, symbols);
+	
+    clearChart();
+    
+    chart(name, symbol, 960, 500);
 }
 
 
